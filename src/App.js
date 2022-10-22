a5174118bbd51fed8cc40d08e62ea1be669f6d79
onefootfarina <clichecontent@gmail.com>
2022-10-21 20:54:54 -0400

    visually represented state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import "normalize.css";
+
+// arrays with a array, strings null, "F", "S"
 
 function App() {
   let [grid, setGrid] = useState([
-    [],
-    [],
-  ])
+    [null, null, null, "F"],
+    [null, null, null, null],
+    [null, null, null, null],
+    ["S", null, null, null],
+  ]);
+  // console.log(grid);
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+    <>
+      <h1>hello</h1>
+      <div className="gridContainer">
+        {grid.map((row) => {
+          return (
+            <div className="gridRow">
+              {row.map((gridItem) => {
+                if (gridItem === null) {
+                  return (
+                    <div className="box">
+                      <p>GRID</p>
+                    </div>
+                  );
+                } else if (gridItem === "F") {
+                  return (
+                    <div className="box">
+                      <p>FOOD</p>
+                    </div>
+                  );
+                } else if (gridItem === "S") {
+                  return (
+                    <div className="box">
+                      <p>SNAKE</p>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
