80f991dcaf575d38ad9f815aa458f260671b8b18
onefootfarina <clichecontent@gmail.com>
2022-10-22 09:51:40 -0400

    got the info where snake head is, have 2 find tail

--- src/moveSnake.js
+++ src/moveSnake.js
@@ -1,4 +1,9 @@
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+
 export default function moveSnake(grid, direction) {
   let newGrid = grid;
-  return newGrid;
+  // return newGrid;
+  for (row in grid) {
+    console.log(row);
+  }
 }
