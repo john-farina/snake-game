80150255bfed2e405d5e52724d672451b1a58628
onefootfarina <clichecontent@gmail.com>
2022-10-24 11:40:49 -0400

    passed all tests with all directions looking good

--- src/App.test.js
+++ src/App.test.js
@@ -8,26 +8,68 @@ import moveSnakeAndDrawGrid from "./moveSnakeAndDrawGrid";
 // w  2 [null, null, null, null],
 //    3 [null, null, null, null],
 
-// test("should move one space to right", () => {
-//   let snakeCoords = [
-//     [2, 2],
-//     [2, 1],
-//     [2, 0],
-//   ];
-//   // - - - -
-//   // - - - -
-//   // T B H -
-//   // - - - -
+test("should move one space down", () => {
+  let snakeCoords = [
+    [2, 3],
+    [1, 3],
+    [0, 3],
+  ];
+  //0 - - - T
+  //1 - - - B
+  //2 - - - H
+  //3 - - - -
 
-//   let movedGrid = [
-//     [null, null, null, null],
-//     [null, null, null, null],
-//     [null, "TAIL", "BODY", "HEAD"],
-//     [null, null, null, null],
-//   ];
+  let movedGrid = [
+    [null, null, null, null],
+    [null, null, null, "TAIL"],
+    [null, null, null, "BODY"],
+    [null, null, null, "HEAD"],
+  ];
 
-//   expect(moveSnakeAndDrawGrid(snakeCoords, "right")).toStrictEqual(movedGrid);
-// });
+  expect(moveSnakeAndDrawGrid(snakeCoords, "down")).toStrictEqual(movedGrid);
+});
+
+test("should move one space up", () => {
+  let snakeCoords = [
+    [1, 3],
+    [2, 3],
+    [3, 3],
+  ];
+  //0 - - - -
+  //1 - - - H
+  //2 - - - B
+  //3 - - - T
+
+  let movedGrid = [
+    [null, null, null, "HEAD"],
+    [null, null, null, "BODY"],
+    [null, null, null, "TAIL"],
+    [null, null, null, null],
+  ];
+
+  expect(moveSnakeAndDrawGrid(snakeCoords, "up")).toStrictEqual(movedGrid);
+});
+
+test("should move one space to right", () => {
+  let snakeCoords = [
+    [2, 2],
+    [2, 1],
+    [2, 0],
+  ];
+  // - - - -
+  // - - - -
+  // T B H -
+  // - - - -
+
+  let movedGrid = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, "TAIL", "BODY", "HEAD"],
+    [null, null, null, null],
+  ];
+
+  expect(moveSnakeAndDrawGrid(snakeCoords, "right")).toStrictEqual(movedGrid);
+});
 
 test("should move one space to left", () => {
   let snakeCoords = [
@@ -49,5 +91,3 @@ test("should move one space to left", () => {
 
   expect(moveSnakeAndDrawGrid(snakeCoords, "left")).toStrictEqual(movedGrid);
 });
-
-//make a function that draws snake and gameboard
--- src/moveSnakeAndDrawGrid.js
+++ src/moveSnakeAndDrawGrid.js
@@ -12,7 +12,6 @@ function drawSnakeInGrid(snakeCoord) {
   for (let i = 0; i < snakeCoord.length; i++) {
     let rowCoord = snakeCoord[i][0];
     let columnCoord = snakeCoord[i][1];
-    console.log("drawSnakeInGridFunction", i, snakeCoord[i]);
 
     //if (first element in array?) = Head
     if (i === 0) {
@@ -25,43 +24,83 @@ function drawSnakeInGrid(snakeCoord) {
     }
   }
 
+  // log table (breaks node)
+  // for (row of grid) {
+  //   console.log(row);
+  // }
+
   return grid;
 }
 
 export default function moveSnakeAndDrawGrid(snakeCoords, direction) {
   let snakeCoord = _.cloneDeep(snakeCoords);
 
-  //first grid test (using default coord - no moving)
+  // first grid test (using default coord - no moving)
   drawSnakeInGrid(snakeCoord);
 
-  if (direction === "left") {
-    // Remove LAST
-    snakeCoord.pop();
+  //remove tail in array then place new head in front
+  snakeCoord.pop();
 
+  if (direction === "left") {
     // place FIRST: get head row & get head column - 1 = (left)
     snakeCoord.unshift([snakeCoord[0][0], snakeCoord[0][1] - 1]);
 
-    console.log("after the shift to left");
+    console.log("Left");
     return drawSnakeInGrid(snakeCoord);
   }
+
   if (direction === "right") {
-    // Remove LAST
-    snakeCoord.pop();
+    // take head coord and add one to column (right)
+    snakeCoord.unshift([snakeCoord[0][0], snakeCoord[0][1] + 1]);
+
+    console.log("Right");
+    return drawSnakeInGrid(snakeCoord);
+  }
+
+  if (direction === "up") {
+    // get head row - 1 (up); take head column num (default for up)
+    snakeCoord.unshift([snakeCoord[0][0] - 1, snakeCoord[0][1]]);
+
+    console.log("Up");
+    return drawSnakeInGrid(snakeCoord);
+  }
+
+  if (direction === "down") {
+    // get head row + 1 (down); take head column num (default for down)
+    snakeCoord.unshift([snakeCoord[0][0] + 1, snakeCoord[0][1]]);
+
+    console.log("Down");
     return drawSnakeInGrid(snakeCoord);
   }
 }
 
-///////// (!NODE TESTS!)
+/////////////////////// (!NODE TESTS!)
 // each row is 0-4
+
 // let snakeCoords = [
 //   [2, 1],
 //   [2, 2],
 //   [2, 3],
 // ];
 // moveSnakeAndDrawGrid(snakeCoords, "left");
-let coord = [
-  [2, 2],
-  [2, 1],
-  [2, 0],
-];
-moveSnakeAndDrawGrid(coord, "left");
+
+// let coord = [
+//   [2, 2],
+//   [2, 1],
+//   [2, 0],
+// ];
+// moveSnakeAndDrawGrid(coord, "right");
+
+// let coords = [
+//   [1, 3],
+//   [2, 3],
+//   [3, 3],
+// ];
+// moveSnakeAndDrawGrid(coords, "up");
+
+// let coords = [
+//   [2, 3],
+//   [1, 3],
+//   [0, 3],
+// ];
+// moveSnakeAndDrawGrid(coords, "down");
